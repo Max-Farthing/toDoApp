76434835b28d9bd5e0afccc9f3ada5620c774cfd
mafarthi <maxfarthing1@gmail.com>
2024-06-24 14:25:39 -0400

    added env variables for DB Connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,32 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body')
+
 const app = express()
 
-app.get("/api", (req, res) => {
-    res.json({"test": ["Testing info sharing"]})
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    )
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
 })
 
-app.listen(5000, () => console.log("server started on port 5000"))
+app.get("/", (req, res) => {
+    res.json({ "test": ["Testing info sharing"] })
+})
+
+const databaseConnection = process.env.DATABASE_URL
+mongoose
+    .connect(
+        databaseConnection
+    )
+    .then(result => {
+        app.listen(5000, () => console.log("server started on port 5000"))
+    })
+    .catch(err => console.log(err))
