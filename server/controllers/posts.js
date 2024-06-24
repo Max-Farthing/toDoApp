b5dd8dea13ca1b7a97ee89303721f72bde60e01a
mafarthi <maxfarthing1@gmail.com>
2024-06-24 15:49:32 -0400

    trying to add controllers/models

---
+++ server/controllers/posts.js
@@ -0,0 +1,22 @@
+const Task = require('../models/task')
+const User = require('../models/user')
+
+exports.getPosts = (req, res, next) => {
+
+}
+
+exports.createPost = (req, res, next) => {
+
+}
+
+exports.getPost = (req, res, next) => {
+
+}
+
+exports.updatePost = (req, res, next) => {
+
+}
+
+exports.deletePost = (req, res, next) => {
+    
+}
---
+++ server/models/task.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const taskSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        steps: [],
+        creator: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model("Task", taskSchema)
